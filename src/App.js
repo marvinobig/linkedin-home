2b86196a49e722dbdd90976cf02b78f3863fdcf8
marvin obig <obig.marv@gmail.com>
2022-03-09 16:33:35 +0000

    imported navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import NavBar from './components/Navigation';
 
 export default function App() {
     return (
         <div>
-            
+            <NavBar />
         </div>
     )
 }
