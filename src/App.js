1c88dc152884d11e1c0bc7c9211373a9f7502c7d
marvin obig <obig.marv@gmail.com>
2022-03-10 16:22:49 +0000

    imported user & news card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import NavBar from './components/Navigation';
 import StartPost from './components/StartPostCard';
+import UserCard from './components/UserCard';
+import News from './components/NewsCard';
 
 export default function App() {
     return (
         <div className='app-container'>
             <NavBar />
             <StartPost />
+            <UserCard />
+            <News />
         </div>
     )
 }
