68da01af4c5e93f0b46ec77c496a035e7a1e4593
marvin obig <obig.marv@gmail.com>
2022-03-10 09:43:31 +0000

    added start post component

---
+++ src/components/StartPostCard.jsx
@@ -0,0 +1,19 @@
+export default function StartPost() {
+    return (
+        <section>
+            <div id="post">
+                <div id="me-icon">
+                    <i className="material-icons">account</i>
+                </div>
+                <i className="material-icons"></i>
+                <input type="text" />
+            </div>
+            <div id="post-actions">
+                <p><i className="material-icons"></i> Photo</p>
+                <p><i className="material-icons"></i> Video</p>
+                <p><i className="material-icons"></i> Event</p>
+                <p><i className="material-icons"></i> Write article</p>
+            </div>
+        </section>
+    )
+}
