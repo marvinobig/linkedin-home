c1e5f05133f87c303f8268af8a19ada13ac9f965
marvin obig <obig.marv@gmail.com>
2022-03-10 16:11:59 +0000

    finished news card

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,51 @@
+export default function News() {
+    return (
+        <section id="news">
+            <div id="title">
+                <p>LinkedIn News</p>
+                <i className="material-icons">priority_high</i>
+            </div>
+            <div className="item">
+                <div className="list">
+                    <i className="material-icons">radio_button_unchecked</i>
+                    <p>elementum sagittis vitae et leo duis ut diam quam nulla.</p>
+                </div>
+                <p>6d ago</p>
+            </div>
+            <div className="item">
+                <div className="list">
+                    <i className="material-icons">radio_button_unchecked</i>
+                    <p>elementum sagittis vitae et leo duis ut diam quam nulla.</p>
+                </div>
+                <p>6d ago</p>
+            </div>
+            <div className="item">
+                <div className="list">
+                    <i className="material-icons">radio_button_unchecked</i>
+                    <p>elementum sagittis vitae et leo duis ut diam quam nulla.</p>
+                </div>
+                <p>6d ago</p>
+            </div>
+            <div className="item">
+                <div className="list">
+                    <i className="material-icons">radio_button_unchecked</i>
+                    <p>elementum sagittis vitae et leo duis ut diam quam nulla.</p>
+                </div>
+                <p>6d ago</p>
+            </div>
+            <div className="item">
+                <div className="list">
+                    <i className="material-icons">radio_button_unchecked</i>
+                    <p>elementum sagittis vitae et leo duis ut diam quam nulla.</p>
+                </div>
+                <p>6d ago</p>
+            </div>
+            <div className="item">
+                <div className="show-more">
+                    <p>Show more</p>
+                    <i className="material-icons">expand_more</i>
+                </div>
+            </div>
+        </section>
+    )
+}
