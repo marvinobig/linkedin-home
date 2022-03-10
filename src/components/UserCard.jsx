51e5b8ebd25e69e3520f2293969c12e21b21a152
marvin obig <obig.marv@gmail.com>
2022-03-10 14:14:42 +0000

    finished user card

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,23 @@
+export default function UserCard() {
+    return (
+        <section id="user-card">
+            <div className="user-cover"></div>
+            <div className="user"></div>
+            <p>Marv Obig</p>
+            <p>Frontend Developer</p>
+            <hr />
+            <div id="connections">
+                <div id="current-connections">
+                    <p>Connections</p>
+                    <p id="num-of-connections">15</p>
+                </div>
+                <p>Grow your network</p>
+                <div id="views">
+                    <p>Exclusive tools & insights</p>
+                    <p>Reactivate premium</p>
+                </div>
+                <p id="items"><i className="material-icons">bookmark</i>My items</p>
+            </div>
+        </section>
+    )
+}
