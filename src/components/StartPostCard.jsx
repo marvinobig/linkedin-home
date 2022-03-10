68699d3a48de6c887b493605f2b175c51003996f
marvin obig <obig.marv@gmail.com>
2022-03-10 10:33:21 +0000

    finished start post section

--- src/components/StartPostCard.jsx
+++ src/components/StartPostCard.jsx
@@ -1,18 +1,18 @@
 export default function StartPost() {
     return (
-        <section>
+        <section id="post-add">
             <div id="post">
-                <div id="me-icon">
-                    <i className="material-icons">account</i>
+                <i className="material-icons">account_circle</i>
+                <div id="write-post">
+                    <label for="post-input" className="material-icons">post_add</label>
+                    <input id="post-input" type="text" placeholder="Start a post"/>
                 </div>
-                <i className="material-icons"></i>
-                <input type="text" />
             </div>
             <div id="post-actions">
-                <p><i className="material-icons"></i> Photo</p>
-                <p><i className="material-icons"></i> Video</p>
-                <p><i className="material-icons"></i> Event</p>
-                <p><i className="material-icons"></i> Write article</p>
+                <p><i className="material-icons">image</i> Photo</p>
+                <p><i className="material-icons">slideshow</i> Video</p>
+                <p><i className="material-icons">calendar_month</i> Event</p>
+                <p><i className="material-icons">article</i> Write article</p>
             </div>
         </section>
     )
