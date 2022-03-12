2921f3eda2069aa87543e06005bcb726ffea23ce
marvin obig <obig.marv@gmail.com>
2022-03-12 18:19:18 +0000

    seperated render post logic to seperate file

--- src/App.js
+++ src/App.js
@@ -3,27 +3,16 @@ import NavBar from "./components/Navigation";
 import StartPost from "./components/StartPostCard";
 import UserCard from "./components/UserCard";
 import News from "./components/NewsCard";
-import Post from "./components/UserPostCard";
-import PostData from "./postData";
+import {PostData} from "./postData";
 
 export default function App() {
-  const POST = PostData.map((post) => {
-    return (
-      <Post
-        user={post.user}
-        text={post.text}
-        img={post.img}
-        followers={post.followers}
-      />
-    );
-  });
   return (
     <div className="app-container">
       <NavBar />
       <StartPost />
       <UserCard />
       <News />
-      {POST}
+      {PostData}
     </div>
   );
 }
--- src/postData.js
+++ src/postData.js
@@ -1,30 +1,44 @@
-export default [
-    {
-        user : "Marv Obig",
-        text : "This is my first post. I am a react developer",
-        img : "linkedin-react.png",
-        followers : 20
-    },
+import Post from "./components/UserPostCard";
 
-    {
-        user : "Sim Bai",
-        text : "I am a graphic designer with next level skills",
-        img : "linkedin-react.png",
-        followers : 40
-    },
+const POST = [
+  {
+    user: "Marv Obig",
+    text: "This is my first post. I am a react developer",
+    img: "linkedin-react.png",
+    followers: 20,
+  },
 
-    {
-        user : "datOne",
-        text : "I c u",
-        img : "",
-        followers : 13
-    },
+  {
+    user: "Sim Bai",
+    text: "I am a graphic designer with next level skills",
+    img: "linkedin-react.png",
+    followers: 40,
+  },
 
-    {
-        user : "freeCode",
-        text : "Skills a fullstack JavaScript developer needs to have",
-        img : "",
-        followers : 100000
-    }
+  {
+    user: "datOne",
+    text: "I c u",
+    img: "",
+    followers: 13,
+  },
 
-]
+  {
+    user: "freeCode",
+    text: "Skills a fullstack JavaScript developer needs to have",
+    img: "",
+    followers: 100000,
+  },
+];
+
+const PostData = POST.map((post) => {
+  return (
+    <Post
+      user={post.user}
+      text={post.text}
+      img={post.img}
+      followers={post.followers}
+    />
+  );
+});
+
+export { PostData };
