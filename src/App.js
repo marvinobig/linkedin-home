293fddd7c8970bc448f8205b65e0379e5e307764
marvin obig <obig.marv@gmail.com>
2022-03-12 16:29:16 +0000

    used .map to map each post object property to the post component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import './App.css';
-import NavBar from './components/Navigation';
-import StartPost from './components/StartPostCard';
-import UserCard from './components/UserCard';
-import News from './components/NewsCard';
-import Post from './components/UserPostCard';
+import "./App.css";
+import NavBar from "./components/Navigation";
+import StartPost from "./components/StartPostCard";
+import UserCard from "./components/UserCard";
+import News from "./components/NewsCard";
+import Post from "./components/UserPostCard";
+import PostData from "./postData";
 
 export default function App() {
+  const POST = PostData.map((post) => {
     return (
-        <div className='app-container'>
-            <NavBar />
-            <StartPost />
-            <UserCard />
-            <News />
-            <Post 
-                user = "Marv Obig"
-                text = "This is my first post. I am a react developer"
-                img = "linkedin-react.png"
-                followers = {20}
-            />
-            <Post 
-                user = "Sim Bai"
-                text = "I am small and very cute but one thing that angers me is when people pay too much attention to my height"
-                img = "linkedin-guide-image.png"
-                followers = {40}
-            />
-            <Post 
-                user = "DatOne"
-                text = "Sometimes, I lie awake at night to listen to my next door neighbours eating."
-                followers = {1}
-            />
-        </div>
-    )
+      <Post
+        user={post.user}
+        text={post.text}
+        img={post.img}
+        followers={post.followers}
+      />
+    );
+  });
+  return (
+    <div className="app-container">
+      <NavBar />
+      <StartPost />
+      <UserCard />
+      <News />
+      {POST}
+    </div>
+  );
 }
---
+++ src/postData.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        user : "Marv Obig",
+        text : "This is my first post. I am a react developer",
+        img : "linkedin-react.png",
+        followers : 20
+    },
+
+    {
+        user : "Sim Bai",
+        text : "I am a graphic designer with next level skills",
+        img : "linkedin-react.png",
+        followers : 40
+    },
+
+    {
+        user : "datOne",
+        text : "I c u",
+        img : "",
+        followers : 13
+    },
+
+    {
+        user : "freeCode",
+        text : "Skills a fullstack JavaScript developer needs to have",
+        img : "",
+        followers : 100000
+    }
+
+]
